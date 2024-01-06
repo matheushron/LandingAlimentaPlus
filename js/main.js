f119a0cc0e90c42c2c3f4e72aeec50ca100e5b62
zJvco <65981077+zJvco@users.noreply.github.com>
2024-01-06 17:11:10 -0300

    Feita as novas alterações

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
 window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("myHeader");
+var links = document.querySelectorAll(".nav-link");
+var loginText = document.querySelector(".LoginText")
 var sticky = header.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
+    links.forEach(item => {
+      item.classList.add("change-color")
+    })
+    loginText.classList.add("change-color")
   } else {
     header.classList.remove("sticky");
+    links.forEach(item => {
+      item.classList.remove("change-color")
+    })
+    loginText.classList.remove("change-color")
   }
 }
